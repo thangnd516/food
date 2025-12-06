0709f66d05a353cf64a5e7ad07b52fddbc88d640
thang516 <nguyenthangfa2001@gmail.com>
2025-12-06 22:30:31 +0700

    up

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -7,4 +7,25 @@ import bcrypt from "bcrypt"
 export async function POST() {
     const body = await req.json();
     const { name, email, password } = body;
+
+    try {
+        const existingUser = await UserActivation.findOne({ email })
+        if (existingUser) {
+            return NextResponse.json(
+                { err: "email already in use" },
+                { status: 500 }
+            );
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await new user({
+            name, email, password: hashedPassword
+        }).save()
+        return NextResponse.json({ msg: "user registered successfully" }, { status: 200 })
+
+    } catch (error) {
+        console.log("error", error);
+        return NextResponse.json({ err: error.message }, { status: 500 });
+    }
+
+
 }
