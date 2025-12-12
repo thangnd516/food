05860799b727f0dbd55b82a08ca2a0cb5288063d
thang516 <nguyenthangfa2001@gmail.com>
2025-12-12 20:07:05 +0700

    change user

--- model/User.js
+++ model/User.js
@@ -1,38 +1,26 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+}, {
+  timestamps: true,
+});
 
-    name: {
-        type: String,
-        trim: true,
-        minLength: 3,
-        maxLength: 20,
-    },
-    email: {
-        type: String,
-        require: true,
-        trim: true,
-        unique: true,
-        index: true,
-        lowercase: true,
-    },
-    image: {
-        type: String,
-        default: "https://placehold.co/600x400.png",
-        password: {
-            type: String
-        },
-        role: {
-            type: String,
-            enum: ["user", "admin"],
-            default: "user",
-            require: true,
-        }
-    }
-},
-    {
-        timestamps: true
-    }
-);
-
-export default mongoose.models.User || mongoose.model("User", UserSchema);
+export default mongoose.models.User || mongoose.model('User', UserSchema);
