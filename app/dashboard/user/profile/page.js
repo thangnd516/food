760d082b3edb4975fe29e06574b010649c221135
thang516 <nguyenthangfa2001@gmail.com>
2025-12-14 21:59:34 +0700

    add page 2

---
+++ app/dashboard/user/profile/page.js
@@ -0,0 +1,11 @@
+import VertivalTabs from "@/components/dashboard/user/VerticalTabs";
+
+import PersonolInfo from "@/components/dashboard/user/profile/PersonolInfo";
+
+export default function DashboardPage() {
+  return (
+    <VertivalTabs>
+      <PersonolInfo/>
+    </VertivalTabs>
+  );
+}
