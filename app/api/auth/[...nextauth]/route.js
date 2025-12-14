84d3b3792378485406922085ebffec823a9bdba2
thang516 <nguyenthangfa2001@gmail.com>
2025-12-14 21:58:41 +0700

    up3

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,9 +1,3 @@
-// import NextAuth from "next-auth";
-// import { authOptions } from "../../../../utils/authOptions";
-
-// const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
 import NextAuth from "next-auth";
 import { authOptions } from "@/utils/authOptions";
 
