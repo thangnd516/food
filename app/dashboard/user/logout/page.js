c0e86803775bd5d65d9dc3aa58a9413d6dea5014
thang516 <nguyenthangfa2001@gmail.com>
2025-12-18 22:31:44 +0700

    up

---
+++ app/dashboard/user/logout/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import VerticalTabs from "@/components/dashboard/user/VerticalTabs";
+
+import LogOut from "@/components/dashboard/user/logout/LogOut";
+
+export default function LogOutPage() {
+  return (
+    <VerticalTabs>
+      <LogOut />
+    </VerticalTabs>
+  );
+}
