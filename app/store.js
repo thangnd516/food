b6ba047c8c1264574d5acf50189c5d231288ccdb
thang516 <nguyenthangfa2001@gmail.com>
2025-12-19 22:50:28 +0700

    up

---
+++ app/store.js
@@ -0,0 +1,34 @@
+"use client";
+
+import { configureStore } from "@reduxjs/toolkit";
+
+import sliderReducer from "@/slice/sliderSlice";
+// import categoryReducer from "@/slice/categorySlice";
+// import productReducer from "@/slice/productSlice";
+// import cartReducer from "@/slice/cartSlice";
+// import deliveryAreaReducer from "@/slice/deliveryAreaSlice";
+// import couponReducer from "@/slice/couponSlice";
+// import addressReducer from "@/slice/addressSlice";
+// import reservationTime from "@/slice/reservationtimeSlice";
+// import userdeliveryAreaReducer from "@/slice/userdeliveryAreaSlice";
+export const store = configureStore({
+  reducer: {
+    // reservationTimes: reservationTime,
+
+    sliders: sliderReducer,
+
+    // categories: categoryReducer,
+
+    // products: productReducer,
+
+    // cart: cartReducer,
+
+    // coupons: couponReducer,
+
+    // deliveryAreas: deliveryAreaReducer,
+
+    // addresses: addressReducer,
+
+    // userdeliveryAreas: userdeliveryAreaReducer,
+  },
+});
