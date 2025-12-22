bd53f40dcce259c8c8fc0f09237c57f7a4e5b0f7
thang516 <nguyenthangfa2001@gmail.com>
2025-12-22 21:53:43 +0700

    up

---
+++ app/dashboard/admin/product/size/[id]/page.js
@@ -0,0 +1,8 @@
+"use client";
+import ProductSizes from "@/components/dashboard/admin/product/sizes/ProductSizes";
+
+// In your page component
+
+export default function ProductSize() {
+  return <ProductSizes/>;
+}
