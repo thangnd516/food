1ce8181a3650fd018b2c0cd31aae80392330456f
thang516 <nguyenthangfa2001@gmail.com>
2025-12-22 21:53:37 +0700

    up

---
+++ app/dashboard/admin/product/gallery/[id]/page.js
@@ -0,0 +1,8 @@
+"use client";
+import ProductGallery from "@/components/dashboard/admin/product/gallery/ProductGallery";
+
+// In your page component
+
+export default function ProductGallerys() {
+  return <ProductGallery/>;
+}
