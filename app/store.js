1b7105e4e3983cd4e3d5b53d5196bb98bbd9e71f
thang516 <nguyenthangfa2001@gmail.com>
2025-12-22 21:53:21 +0700

    up

--- app/store.js
+++ app/store.js
@@ -4,7 +4,7 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import sliderReducer from "@/slice/sliderSlice";
 import categoryReducer from "@/slice/categorySlice";
-// import productReducer from "@/slice/productSlice";
+import productReducer from "@/slice/productSlice";
 // import cartReducer from "@/slice/cartSlice";
 // import deliveryAreaReducer from "@/slice/deliveryAreaSlice";
 // import couponReducer from "@/slice/couponSlice";
@@ -19,7 +19,7 @@ export const store = configureStore({
 
     categories: categoryReducer,
 
-    // products: productReducer,
+    products: productReducer,
 
     // cart: cartReducer,
 
