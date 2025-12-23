2213ff8ed760e19e3134750685ba70939f89d1f5
thang516 <nguyenthangfa2001@gmail.com>
2025-12-23 20:52:11 +0700

    up

---
+++ app/dashboard/admin/product/option/[id]/page.js
@@ -0,0 +1,8 @@
+"use client";
+import ProductOptions from "@/components/dashboard/admin/product/options/ProductOptions";
+
+// In your page component
+
+export default function ProductOptionsPage() {
+    return <ProductOptions />;
+}
