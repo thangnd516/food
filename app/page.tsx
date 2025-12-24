724823764356fddc1bdc9e09aef575149816719c
thang516 <nguyenthangfa2001@gmail.com>
2025-12-24 21:52:52 +0700

    up

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import HomeComponent from "@/components/home/Home";
+import Menu from "@/components/menu/Menu";
 export default function Home() {
   return (
     <>
       <HomeComponent />
+      <Menu />
     </>
   );
 }
