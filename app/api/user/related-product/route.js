96fb2ca4aa74831fc96e57cec4451872233f6df3
thang516 <nguyenthangfa2001@gmail.com>
2026-01-03 21:35:45 +0700

    up

---
+++ app/api/user/related-product/route.js
@@ -0,0 +1,7 @@
+
+import { NextResponse } from "next/server";
+import dbConnect from "@/utils/dbConnect";
+import Product from "@/model/product";
+import ProductGallery from "@/model/productgallery";
+import ProductOption from "@/model/productoption";
+import ProductSize from "@/model/productsize";
