43d82994b56f739251cf8972e2fd7c6357076934
thang516 <nguyenthangfa2001@gmail.com>
2026-01-04 22:24:13 +0700

    up

---
+++ app/dashboard/admin/coupon/create/page.js
@@ -0,0 +1,8 @@
+"use client";
+import CouponForm from "@/components/dashboard/admin/coupon/create/CouponForm";
+
+// In your page component
+
+export default function CouponCreate() {
+  return <CouponForm/>;
+}
