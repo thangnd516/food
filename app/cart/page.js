7efbc1b3de12038f1ce7129eedf6b767ef16cfa1
thang516 <nguyenthangfa2001@gmail.com>
2026-01-07 20:44:37 +0700

    up

---
+++ app/cart/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import Top from "@/components/topimage/Top";
+
+import Cart from "@/components/cart/Cart";
+
+export default function ProdcutCart() {
+  return (
+    <>
+      <Top />
+      <Cart />
+    </>
+  );
+}
