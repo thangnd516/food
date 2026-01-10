06a832870bc89fa8213a5b028eb86ef803a64a56
thang516 <nguyenthangfa2001@gmail.com>
2026-01-10 22:09:29 +0700

    up

---
+++ app/checkout/page.js
@@ -0,0 +1,10 @@
+"use client";
+import Top from "@/components/topimage/Top";
+import Checkout from "@/components/checkout/Checkout";
+export default function ProductView() {
+  return (
+    <>
+      <Checkout />
+    </>
+  );
+}
